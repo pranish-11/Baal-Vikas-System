1dbae9b0289e5d0e894c20c6961434a66ba4b6bb
prithakmanandhar11 <prithakmanandhar11@gmail.com>
2026-05-24 12:27:47 +0545

    Rename Baal Vikas and save changes before worktree merge

--- Baal Vikas/server/src/routes/activities.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { listActivities, createNewActivity } = require("../controllers/activityController");
-
-const router = express.Router();
-
-router.get("/", listActivities);
-router.post("/", createNewActivity);
-
-module.exports = router;
